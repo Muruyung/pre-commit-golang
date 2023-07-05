d5d9f910e129954b9c0be43d19173a0d5b789515
Robi Naufal Kaosar <Robi.kaosar@salt.co.id>
2023-07-05 10:12:29 +0700

    feat: add some detail error for unit test

--- run-go-unit-tests.sh
+++ run-go-unit-tests.sh
@@ -1,9 +1,29 @@
 #!/usr/bin/env bash
 
 fail() {
-  echo 'unit tests failed, run first: go test --tags=!integration ./... -coverprofile=coverage.cov ./...'
+  echo 'unit tests failed'
+  exit 1
+}
+
+noCoverage() {
+  echo 'coverage.cov file not found. Run first: \033[4;33m go test --tags=!integration ./... -coverprofile=coverage.cov ./... \033[0;37m'
+  exit 1
+}
+
+noTreshold() {
+  echo '.testcoverage.yml file not found. Create first that file following this sample https://github.com/vladopajic/go-test-coverage/blob/main/.testcoverage.example.yml'
   exit 1
 }
 
 # go test --tags=!integration ./... -coverprofile=coverage.cov ./... || fail
+if [ ! -f coverage.cov ]
+then
+  noCoverage
+fi
+
+if [ ! -f .testcoverage.yml ]
+then
+  noTreshold
+fi
+
 go-test-coverage -config .testcoverage.yml || fail
